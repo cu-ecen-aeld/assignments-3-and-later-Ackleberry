e08f1a483de222dd2e8b5e3660dd147be254634c
Brooks Anderson <brooks.anderson@snapone.com>
2024-11-13 09:01:52 -0600

    Make the server handle each client in a unique thread

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,6 +1,6 @@
-/*
-** server.c -- a stream socket server demo
-*/
+/** 
+ * server.c -- a stream socket server demo
+**/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,11 +15,25 @@
 #include <sys/wait.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include "queue.h"
 
 #define PORT "9000"  // the port users will be connecting to
 #define BACKLOG 10	 // how many pending connections queue will hold
 #define FILENAME "/var/tmp/aesdsocketdata"
 #define INITIAL_RX_BUF_SIZE 256
+
+typedef struct client_info_s {
+    int fd;
+    bool is_complete;
+    pthread_t t_id;
+    char addr_str[INET6_ADDRSTRLEN];
+} client_info_t;
+
+typedef struct slist_data_s slist_data_t;
+struct slist_data_s {
+    client_info_t *c_info;
+    SLIST_ENTRY(slist_data_s) entries;
+};
 
 int _sockfd = -1;
 
@@ -27,7 +41,7 @@ void _send_file(int client_socket, const char *filepath) {
 
     FILE *file = fopen(filepath, "r");
     if (file == NULL) {
-		syslog(LOG_ERR, "Error opening file: '%s'\n", filepath);
+        syslog(LOG_ERR, "Error opening file: '%s'\n", filepath);
         return;
     }
 
@@ -49,7 +63,7 @@ char *_receive_message(int client_socket) {
 
     char *buffer = malloc(INITIAL_RX_BUF_SIZE);
     if (!buffer) {
-		syslog(LOG_ERR, "Failed to allocate message buffer\n");
+        syslog(LOG_ERR, "Failed to allocate message buffer\n");
         return NULL;
     }
 
@@ -73,7 +87,7 @@ char *_receive_message(int client_socket) {
             buf_size *= 2; // Double the size of the buffer
             char* new_buffer = realloc(buffer, buf_size);
             if (!new_buffer) {
-				syslog(LOG_ERR, "Failed to reallocate message buffer\n");
+                syslog(LOG_ERR, "Failed to reallocate message buffer\n");
                 free(buffer);
                 return NULL;
             }
@@ -88,142 +102,141 @@ char *_receive_message(int client_socket) {
 int _append_str_to_file(const char *str, const char *filename) {
     FILE *file = fopen(filename, "a");
     if (file == NULL) {
-		syslog(LOG_ERR, "Failed to open file '%s'", filename);
+        syslog(LOG_ERR, "Failed to open file '%s'", filename);
         return -1;
     }
 
     if (fputs(str, file) == EOF) {
-		syslog(LOG_ERR, "Failed to write to file '%s'", filename);
+        syslog(LOG_ERR, "Failed to write to file '%s'", filename);
         fclose(file);
         return -1;
     }
 
     if (fclose(file) != 0) {
-		syslog(LOG_ERR, "Failed to close file '%s'", filename);
+        syslog(LOG_ERR, "Failed to close file '%s'", filename);
         return -1;
     }
 
     return 0;
 }
 
-static int _client_handler(int client_fd) 
+void *_client_handler(void *arg) 
 {
-	char *msg = _receive_message(client_fd);
-	if (msg) {
-		syslog(LOG_DEBUG, "Received: '%s'\n", msg);
-	} else {
-		syslog(LOG_DEBUG, "Error receiving message\n");
-	}
+    client_info_t *c_info = (client_info_t *)arg;
 
-	_append_str_to_file(msg, FILENAME);
-	free(msg);
+    char *msg = _receive_message(c_info->fd);
+    if (msg) {
+        syslog(LOG_DEBUG, "Received: '%s'\n", msg);
+        _append_str_to_file(msg, FILENAME);
+        free(msg);
+        _send_file(c_info->fd, FILENAME);
+    } else {
+        syslog(LOG_DEBUG, "Error receiving message\n");
+    }
 
-	_send_file(client_fd, FILENAME);
-
-	return 0;
+    c_info->is_complete = true;
+    return NULL;
 }
 
 // get sockaddr, IPv4 or IPv6:
 static void *_get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 void _sig_handler(int sig)
 {
     int saved_errno = errno;
 
-	if (sig == SIGINT || sig == SIGTERM) {
-		syslog(LOG_ERR, "Caught signal, exiting");
+    if (sig == SIGINT || sig == SIGTERM) {
+        syslog(LOG_ERR, "Caught signal, exiting...");
         if (_sockfd != -1) {
             close(_sockfd);
         }
-    	remove(FILENAME);
-	}
+        remove(FILENAME);
+    }
 
     errno = saved_errno;
-	exit(0);
+    exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-	struct addrinfo hints;
-	struct addrinfo *servinfo;
-	struct addrinfo *p;
-	struct sockaddr_storage their_addr; // connector's address information
-	socklen_t sin_size;
-	int yes = 1;
-	char s[INET6_ADDRSTRLEN];
-	int rv;
-	bool is_daemon = false;
+    int rv;
 
-	if ((argc == 2) && (strcmp(argv[1], "-d") == 0)) {
-		printf("Running server as daemon...\n");
-		is_daemon = true;
-	}
+    bool is_daemon = false;
+    if ((argc == 2) && (strcmp(argv[1], "-d") == 0)) {
+        printf("Running server as daemon...\n");
+        is_daemon = true;
+    }
 
-	// Hints imposes restrictions on what addrinfo structs are returned.
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
-	hints.ai_socktype = SOCK_STREAM; // TCP
-	hints.ai_flags = AI_PASSIVE; // use my IP
+    // Hints suggests restrictions on what getaddrinfo() structs are returned.
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_flags = AI_PASSIVE; // For wildcard IP address
 
-	// Get a linked list of addrinfo structs
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return -1;
-	}
+    // Get a linked list of addrinfo structs
+    struct addrinfo *servinfo;
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return -1;
+    }
 
-	// loop through all the results and bind to the first we can
-	for (p = servinfo; p != NULL; p = p->ai_next) {
-		// Attempt to create a socket endpoint...
-		if ((_sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			perror("server: socket");
-			continue;
-		}
+    // loop through all the address structs and bind to the first we can
+    struct addrinfo *p;
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        // Attempt to create a socket endpoint...
+        if ((_sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
 
-		// Endpoint created! Now set its options...
-		// SOL_SOCKET - Manipulate options at the sockets API level
-		// SO_REUSEADDR - Allow sockets to bind to a port that still isn't fully released by the OS
-		if (setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-			perror("setsockopt");
-			return -1;
-		}
+        // Endpoint created! Now set its options...
+        // SOL_SOCKET - Manipulate options at the sockets API level
+        // SO_REUSEADDR - Allow sockets to bind to a port that still isn't fully released by the OS
+        int yes = 1;
+        if (setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("setsockopt");
+            return -1;
+        }
 
-		// Assign an address to the socket
-		if (bind(_sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(_sockfd);
-			perror("server: bind");
-			continue;
-		}
+        // Assign an address to the socket
+        if (bind(_sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(_sockfd);
+            perror("server: bind");
+            continue;
+        }
 
-		break;
-	}
+        break; /* Success! */
+    }
 
-	freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo);
 
-	if (p == NULL)  {
-		fprintf(stderr, "server: failed to bind\n");
-		return -1;
-	}
+    /* Make sure we didn't iterate through the whole address list */
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        return -1;
+    }
 
-	if (listen(_sockfd, BACKLOG) == -1) {
-		syslog(LOG_ERR, "listen() failed!");
-		return -1;
-	}
+    if (listen(_sockfd, BACKLOG) == -1) {
+        syslog(LOG_ERR, "listen() failed!");
+        return -1;
+    }
 
-	if (is_daemon) {
-		if (daemon(0, 0) == -1) {
-			syslog(LOG_ERR, "Failed to launch daemon!");
-			return -1;
-		}
-	}
+    if (is_daemon) {
+        if (daemon(0, 0) == -1) {
+            syslog(LOG_ERR, "Failed to launch daemon!");
+            return -1;
+        }
+    }
 
-	struct sigaction sa;
+    struct sigaction sa;
     sa.sa_handler = _sig_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
@@ -232,25 +245,60 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-	remove(FILENAME);
-	syslog(LOG_DEBUG, "Listening for a new connection...");
+    remove(FILENAME);
+    syslog(LOG_DEBUG, "Listening for a new connection...");
 
-	while (1) {
-		sin_size = sizeof(their_addr);
-		int client_fd = accept(_sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (client_fd < 0) {
-			syslog(LOG_ERR, "Failed to accept client connection.");
-			continue;
-		}
+    SLIST_HEAD(slisthead, slist_data_s) head;
+    SLIST_INIT(&head);
 
-		inet_ntop(their_addr.ss_family, _get_in_addr((struct sockaddr *)&their_addr), s, sizeof(s));
-		syslog(LOG_DEBUG, "Accepted connection from %s", s);
-		_client_handler(client_fd);
-		close(client_fd);
-		syslog(LOG_DEBUG, "Closed connection from %s", s);
-	}
+    while (1) {
+        struct sockaddr_storage client_addr;
+        socklen_t addr_size = sizeof(client_addr);
+        int client_fd = accept(_sockfd, (struct sockaddr *)&client_addr, &addr_size);
+        if (client_fd < 0) {
+            syslog(LOG_ERR, "Failed to accept client connection.");
+            continue;
+        }
+
+        client_info_t *c_info = malloc(sizeof(client_info_t));
+        c_info->fd = client_fd;
+        c_info->is_complete = false;
+        c_info->t_id = 0;
+
+        inet_ntop(client_addr.ss_family, 
+                  _get_in_addr((struct sockaddr *)&client_addr), 
+                  c_info->addr_str, 
+                  sizeof(c_info->addr_str));
+        syslog(LOG_DEBUG, "Accepted connection from %s", c_info->addr_str);
+
+        // Create thread to handle client
+        if (pthread_create(&c_info->t_id, NULL, _client_handler, (void *)c_info) != 0) {
+            perror("Failed to create thread");
+            close(c_info->fd);
+            free(c_info);
+            continue;
+        }
+
+        // Add this thread to the linked list
+        slist_data_t *d_ptr = malloc(sizeof(slist_data_t));
+        d_ptr->c_info = c_info;
+        SLIST_INSERT_HEAD(&head, d_ptr, entries);
+
+        // Check if any thread has completed, if so free up resources
+        struct slist_data_s *iter, *tmp = NULL;
+        SLIST_FOREACH_SAFE(iter, &head, entries, tmp) {
+            if (iter->c_info->is_complete) {
+                close(iter->c_info->fd);
+                syslog(LOG_DEBUG, "Closed connection from %s", iter->c_info->addr_str);
+                pthread_join(iter->c_info->t_id, NULL);
+                SLIST_REMOVE(&head, iter, slist_data_s, entries);
+                free(iter->c_info);
+                free(iter);
+            }
+        }
+
+    }
 
     close(_sockfd);
-
-	return 0;
+    return 0;
 }
